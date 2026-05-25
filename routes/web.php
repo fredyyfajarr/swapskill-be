9e0139e5d248140b61c3fb0e780931086cf6d805
fredyyfajarr <fredyfajar46@gmail.com>
2026-05-25 10:13:53 +0700

    Add admin login session bridge

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\AdminSessionBridgeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+Route::get('/admin/session-bridge/{user}', AdminSessionBridgeController::class)
+    ->middleware('signed')
+    ->name('admin.session-bridge');
 
 require __DIR__.'/auth.php';
